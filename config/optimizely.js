dc4f8cf215e229e26c2414425ecba3e51ab4a223
Tom Fuertes <tomfuertes@gmail.com>
2018-07-11 16:23:53 -0500

    update optimizely config to auto get

--- config/optimizely.js
+++ config/optimizely.js
@@ -3,13 +3,19 @@ const request = require('request');
 
 let optimizelyClientInstance;
 
-const DATAFIE_URL = 'https://cdn.optimizely.com/public/2489010562/s/11082312092_11082312092.json';
+const DATAFILE_URL = 'https://cdn.optimizely.com/public/2489010562/s/11082312092_11082312092.json';
 const REFRESH_RATE = 60 * 1000; // every minute
 
-setInterval(()=>{
-  request(DATAFIE_URL, (error, response, body) => {
+function refresh() {
+  request(DATAFILE_URL, (error, response, body) => {
     optimizelyClientInstance = optimizely.createInstance({ datafile: body });
   });
-}, 60 * REFRESH_RATE);
+}
 
-module.exports = ()=>{optimizelyClientInstance};
+refresh();
+
+setInterval(refresh, 60 * REFRESH_RATE);
+
+module.exports = () => {
+  return optimizelyClientInstance;
+};
