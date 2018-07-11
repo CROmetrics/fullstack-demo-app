f7ce287fd8a2a3e876aa200c8a37df09ff6f74ce
Tom Fuertes <tomfuertes@gmail.com>
2018-07-11 14:49:16 -0500

    first commit from 'yo express'

---
+++ app/controllers/home.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+module.exports = (app) => {
+  app.use('/', router);
+};
+
+router.get('/', (req, res, next) => {
+  db.Article.findAll().then((articles) => {
+    res.render('index', {
+      title: 'Generator-Express MVC',
+      articles: articles
+    });
+  });
+});
---
+++ config/config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const rootPath = path.normalize(__dirname + '/..');
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'fullstask-demo-app'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/fullstask-demo-app-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'fullstask-demo-app'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/fullstask-demo-app-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'fullstask-demo-app'
+    },
+    port: process.env.PORT || 3000,
+    db: 'postgres://localhost/fullstask-demo-app-production'
+  }
+};
+
+module.exports = config[env];
