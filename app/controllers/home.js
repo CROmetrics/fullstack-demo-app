3d7c6576a4ad985cb123549b29457054110ad659
Tom Fuertes <tomfuertes@gmail.com>
2018-07-12 13:03:01 -0500

    HOME_PAGE_HERO experiment

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -10,15 +10,32 @@ module.exports = (app) => {
 
 router.get('/', (req, res, next) => {
 
+  // Optimizely SDK Helpers
+  const o = getOptimizelyClient();
+  const exp = 'HOME_PAGE_HERO_TEST';
+  const user = [req.cookies.userid, {/*attributes*/}];
+
+  // Determine if we should show the hero
+  const variation = o.activate(exp, ...user);
+
+  console.log('STUFF:', req.cookies.userid, variation);
+
+  // Pass final args to html
   res.render('index', { 
-    title: 'MailChimp Demo'
+    title: 'MailChimp Demo',
+    shouldShowHero: (variation === 'show') ? true : false
   });
 
 });
 
 
 router.get('/discover', (req, res, next) => {
+  
+  // Optimizely SDK Helpers
+  const o = getOptimizelyClient();
+  const user = [req.cookies.userid, {/*attributes*/}];
 
-  res.render('discover', { /* options */ });
+  // Track reaching the next step
+  o.track('DISCOVER_PAGEVIEW', ...user);
 
 });
